0466021b71ec5698da02a08024d79d74c6cd44d8
h.babchia <bobhaytem@gmail.com>
2023-01-19 20:54:25 +0100

    feat(project): initialise project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <button className='square'>
+        
+       </button>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;

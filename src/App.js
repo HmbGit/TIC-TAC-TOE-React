98118da2a94df06f3f59ced7e20cb190fae63a77
h.babchia <bobhaytem@gmail.com>
2023-01-20 14:19:36 +0100

    feat(app): update app component to reach Game

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <button className='square'>
-        
-       </button>
-        
-      </header>
+     <Game />
     </div>
   );
 }
+
 
 export default App;
